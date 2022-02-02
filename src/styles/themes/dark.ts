0dac7a46cf469daca4c9cf50275cc0a74967c294
IanoMaciel <ianomaciel6385@gmail.com>
2022-02-02 10:02:04 -0400

    Created theme dark and light

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'dark',
+    
+    colos: {
+        primary: '#1B1F38',
+        secundary: '#252A48',
+        tertiary: '#313862',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#E5E5E5',
+
+        success: '#4E41F0',
+        info: '#F7931B',
+        warning: '#E44C4E',
+        
+    },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'light',
+    
+    colos: {
+        primary: '#DCDCDC',
+        secundary: '#FFF',
+        tertiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#5B5B5B',
+
+        success: '#03BB85',
+        info: '#3B5998',
+        warning: '#FF6961',
+        
+    },
+};
