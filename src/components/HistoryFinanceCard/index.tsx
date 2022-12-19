5878a0c72bb6b95fb10250a4cda2ad1cfbe24a1b
Iano Maciel <ianomaciel6385@gmail.com>
2022-12-19 14:41:06 -0400

    refactor: refactoring financial movements card

--- src/components/HistoryFinanceCard/index.tsx
+++ src/components/HistoryFinanceCard/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import * as S from './styles'
 
 interface IHistoryFinanceCardProps {
-    cardColor: string;
     tagColor: string;
     title: string;
     subTitle: string;
@@ -11,14 +10,13 @@ interface IHistoryFinanceCardProps {
 }
 
 const HistoryFinanceCard: React.FC<IHistoryFinanceCardProps> = ({
-    cardColor,
     tagColor,
     title,
     subTitle,
     amount
 }) => {
     return(
-        <S.Container color={cardColor}>
+        <S.Container>
             <S.Tag color={tagColor}/>
             <div>
                 <span>{title}</span>
