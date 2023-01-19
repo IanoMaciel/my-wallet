41b28080edbb14dce3c9fef4162885d851a46ffc
Iano Maciel <ianomaciel6385@gmail.com>
2023-01-18 21:56:04 -0400

    dev: created function formateCurrency.ts

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,8 @@
+const formatCurrency = (current : number): string => {
+    return current.toLocaleString(
+        'pt-br',
+        { style: 'current', currency: 'BRL' }
+    ) 
+};
+
+export default formatCurrency;
