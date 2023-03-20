e5d361de1386c63a4376048caba81c2b87c365d3
Iano Maciel <ianomaciel6385@gmail.com>
2023-03-20 10:55:42 -0400

    refactor: erro wiriting description

--- src/components/MessageBox/index.tsx
+++ src/components/MessageBox/index.tsx
@@ -5,17 +5,17 @@ import * as S from './styles'
 
 interface IMessageBoxProps {
     title: string;
-    desciption: string;
+    description: string;
     footerText: string;
     icon: string;
 }
 
-const MessageBox: React.FC<IMessageBoxProps> = ({ title, desciption, footerText, icon }) => {
+const MessageBox: React.FC<IMessageBoxProps> = ({ title, description, footerText, icon }) => {
     return(
         <S.Container>
             <header>
                 <h1>{ title }<img src={ icon } alt={ title }/></h1>
-                <p>{ desciption }</p>
+                <p>{ description }</p>
             </header>
             <footer>
                 <span>{ footerText }</span>
